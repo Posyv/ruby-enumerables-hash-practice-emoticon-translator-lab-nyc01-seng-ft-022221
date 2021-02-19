18feb232c9294a7dc4f05b570e956c0477a63afe
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-19 03:23:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,11 +2,10 @@
 require 'yaml'
 def load_library
   emoticons = YAML.load_file('emoticons.yml')
-  emoticons
 end
 
 def get_japanese_emoticon
-  # code goes here
+  hhh.each{|key,value| value[0] == ":$" ? result = value[1] : ''  }
 end
 
 def get_english_meaning
